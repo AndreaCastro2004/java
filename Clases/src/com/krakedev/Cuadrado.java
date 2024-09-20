1ab75a0429071ccf4ae221dc593ef4162ab23dd4
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-20 16:01:11 -0500

    Se instancia la clase cuadrado con la funcion calcular area

---
+++ Clases/src/com/krakedev/Cuadrado.java
@@ -0,0 +1,15 @@
+package com.krakedev;
+
+public class Cuadrado {
+	//creamos atributo
+	public int lado;
+	
+	//creamos metodos
+	
+	public double calcularArea() {
+		double resultado=lado*lado;
+		return resultado;
+	}
+	
+	
+}
