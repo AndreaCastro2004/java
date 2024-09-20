02838b1455732989aa4e53870ada82a5cd46739c
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-20 15:05:12 -0500

    Se instancia test Rectangulo

---
+++ Clases/src/com/krakedev/Rectangulo.java
@@ -0,0 +1,15 @@
+package com.krakedev;
+
+public class Rectangulo {
+	//Creamos los atributos
+	public int base;
+	public int altura;
+	
+	//creamos los metodos
+	public int calcularArea(){
+		int area;
+		area=base*altura;
+		return area;
+	}
+	
+}
