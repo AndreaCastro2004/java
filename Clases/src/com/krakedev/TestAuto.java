b87223d7c4f2c2eedc5a501708cf249fafba58e4
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-20 21:13:10 -0500

    Se modifica testAuto

--- Clases/src/com/krakedev/TestAuto.java
+++ Clases/src/com/krakedev/TestAuto.java
@@ -7,21 +7,21 @@ public class TestAuto {
 		Auto auto1 = new Auto();
 		Auto auto2 = new Auto();
 		// 2 Modificar los valores
-		auto1.marca = "Toyota";
-		auto1.anio = 2008;
-		auto1.precio = 2000;
+		auto1.setMarca( "Toyota");
+		auto1.setAnio(2008);
+		auto1.setPrecio( 2000);
 
-		auto2.marca = "Chevrolet";
-		auto2.anio = 2009;
-		auto2.precio = 2600;
+		auto2.setMarca( "Chevrolet");
+		auto2.setAnio( 2009);
+		auto2.setPrecio(2600);
 		// 3.-mostrar los valores en consola
-		System.out.println("marca:" + auto1.marca);
-		System.out.println("anio:" + auto1.anio);
-		System.out.println("precio:" + auto1.precio);
+		System.out.println("marca:" + auto1.getMarca());
+		System.out.println("anio:" + auto1.getAnio());
+		System.out.println("precio:" + auto1.getPrecio());
 		System.out.println("------------");
-		System.out.println("marca:" + auto2.marca);
-		System.out.println("anio:" + auto2.anio);
-		System.out.println("precio:" + auto2.precio);
+		System.out.println("marca:" + auto2.getMarca());
+		System.out.println("anio:" + auto2.getAnio());
+		System.out.println("precio:" + auto2.getPrecio());
 
 		
 	}
