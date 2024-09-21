25f2df91413c0b2814df08227aef5c38c5172dbc
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-20 21:52:01 -0500

    Modificador de datos cambiado y agrego set y get en rectangulo

--- Clases/src/com/krakedev/Rectangulo.java
+++ Clases/src/com/krakedev/Rectangulo.java
@@ -2,8 +2,25 @@ package com.krakedev;
 
 public class Rectangulo {
 	// Creamos los atributos
-	public int base;
-	public int altura;
+	private int base;
+	private int altura;
+	
+	
+	public int getBase() {
+		return base;
+	}
+
+	public void setBase(int base) {
+		this.base = base;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
 
 	// creamos los metodos
 	public int calcularArea() {
