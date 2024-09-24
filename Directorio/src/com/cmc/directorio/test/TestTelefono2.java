a72b25c6f3a72190633d0cd163e8a6c889313efb
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-24 16:55:44 -0500

    Se crea la clase TestTelefono2 y se imprime datos telf

--- Directorio/src/com/cmc/directorio/test/TestTelefono2.java
+++ Directorio/src/com/cmc/directorio/test/TestTelefono2.java
@@ -1,11 +1,15 @@
 package com.cmc.directorio.test;
 
+import com.cmc.directorio.entidades.AdminTelefono;
 import com.cmc.directorio.entidades.Telefono;
 
-public class TestTelefono {
+public class TestTelefono2 {
 
 	public static void main(String[] args) {
-		Telefono telf=new Telefono("movi","09941234123",10);
+		Telefono telf =new Telefono("movi","098234234",20);
+		AdminTelefono at=new AdminTelefono();
+		
+		at.activarMensajeria(telf);
 		System.out.println("Datos telefono");
 		System.out.println("Operadora:"+telf.getOperadora());
 		System.out.println("Numero:"+telf.getNumero());
