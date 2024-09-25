11cf6390d82ea8f1fb51550bfe6d2c41765f0697
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-24 19:48:43 -0500

    Se ejecuta activar usuario en testActivos

--- Directorio/src/com/cmc/directorio/test/TestActivos.java
+++ Directorio/src/com/cmc/directorio/test/TestActivos.java
@@ -1,5 +1,6 @@
 package com.cmc.directorio.test;
 
+import com.cmc.directorio.entidades.AdminContactos;
 import com.cmc.directorio.entidades.AdminTelefono;
 import com.cmc.directorio.entidades.Contacto;
 import com.cmc.directorio.entidades.Telefono;
@@ -9,6 +10,7 @@ public class TestActivos {
 	public static void main(String[] args) {
 		Telefono telf=new Telefono ("claro","0125364789",105);
 		AdminTelefono al=new AdminTelefono();
+		AdminContactos alc=new AdminContactos();
 		Contacto c1=new Contacto("Rosita", "Sufragio", telf, 119);
 		
 		Telefono tel=c1.getTelefono();
@@ -24,7 +26,15 @@ public class TestActivos {
 		System.out.println("--------------------");
 		
 		
-		
+		alc.activarUsuario(c1);
+		System.out.println("nombre:"+c1.getNombre());
+		System.out.println("apellido:"+c1.getApellido());
+		System.out.println("activo:"+c1.isActivo());
+		System.out.println("telefono:"+tel.getNumero());
+		System.out.println("peso:"+c1.getPeso());
+		System.out.println("operadora:"+tel.getOperadora());
+		System.out.println("Tiene Whatsapp:"+telf.isTieneWhatsapp());
+		System.out.println("--------------------");
 
 	}
 
