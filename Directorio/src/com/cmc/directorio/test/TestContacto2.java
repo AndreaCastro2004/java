8b5273edefae6b09d4ee322e36eb0e8c121a9c87
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-24 19:28:21 -0500

    se crea testContactos 2 con informacion diferente

---
+++ Directorio/src/com/cmc/directorio/test/TestContacto2.java
@@ -0,0 +1,34 @@
+package com.cmc.directorio.test;
+
+import com.cmc.directorio.entidades.AdminContactos;
+import com.cmc.directorio.entidades.Contacto;
+import com.cmc.directorio.entidades.Telefono;
+
+public class TestContacto2 {
+
+	public static void main(String[] args) {
+		AdminContactos al=new AdminContactos();
+		Telefono telf1=new Telefono ("claro","0987654321",15);
+		Contacto c=new Contacto("Mario", "Bros", telf1, 190);
+		Telefono telf2=new Telefono ("claro","0123456678",90);
+		Contacto c2=new Contacto("Juan", "Escariote", telf2, 60);
+		
+		Contacto p1=al.buscarMasPesado(c, c2);
+		Telefono pe1=p1.getTelefono();
+		
+		System.out.println("--Informacion de contacto Por mas pesado--");
+		System.out.println("nombre:"+p1.getNombre());
+		System.out.println("apellido:"+p1.getApellido());
+		System.out.println("activo:"+p1.isActivo());
+		System.out.println("telefono:"+pe1.getNumero());
+		System.out.println("peso:"+p1.getPeso());
+		System.out.println("operadora:"+pe1.getOperadora());
+		System.out.println("--------------------");
+
+		boolean operadoras=al.compararOperadoras(c, c2);
+		System.out.println("--Tienen la misma operadora--");
+		System.out.println("operadora:"+operadoras);
+
+	}
+
+}
