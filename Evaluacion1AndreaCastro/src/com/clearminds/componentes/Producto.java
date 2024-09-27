43c34f9653454af3a7316e2c4b7789d5539bbfa1
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-27 14:22:23 -0500

    metodo  disminuirPrecio

--- Evaluacion1AndreaCastro/src/com/clearminds/componentes/Producto.java
+++ Evaluacion1AndreaCastro/src/com/clearminds/componentes/Producto.java
@@ -1,5 +1,18 @@
 package com.clearminds.componentes;
 
 public class Producto {
-
+	private String nombre;
+	private double precio;
+	private String codigo;
+	
+	//metodos
+	public void incrementarPrecio( int porcentajeIncremento) {
+			double incremento=porcentajeIncremento/100.0;	
+			double montoIncremento=precio*incremento;
+			precio=precio+montoIncremento;
+		}
+	public void disminuirPrecio(double valorDescuento) {
+		precio=precio-valorDescuento;
+	}
+	
 }
