debb82a04142b0d02cafe14ad6f3007c620ac106
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-27 15:15:00 -0500

    metodo mostrarConfiguraciones

--- Evaluacion1AndreaCastro/src/com/clearminds/maquina/MaquinaDulces.java
+++ Evaluacion1AndreaCastro/src/com/clearminds/maquina/MaquinaDulces.java
@@ -16,4 +16,29 @@ public class MaquinaDulces {
 		Celda celdaCodigo=new Celda(codigo);
 		celdas.add(celdaCodigo);
 	}
+	public void mostrarConfiguracion() {
+		ArrayList<Celda> celda=new ArrayList<Celda>();
+		for(int i=0;i<celdas.size();i++) {
+			System.out.println("Celdas:"+celda.get(i));
+		}
+		
+	}
+
+	public ArrayList<Celda> getCeldas() {
+		return celdas;
+	}
+
+	public void setCeldas(ArrayList<Celda> celdas) {
+		this.celdas = celdas;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
 }
