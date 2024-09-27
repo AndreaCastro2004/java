73a5370463c3c18b10b14d49b0d2a96723ed8a94
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-27 14:44:21 -0500

    Agrego TestCelda he indico el nullPointerException

--- Evaluacion1AndreaCastro/src/com/clearminds/componentes/Celda.java
+++ Evaluacion1AndreaCastro/src/com/clearminds/componentes/Celda.java
@@ -15,6 +15,24 @@ public class Celda {
 			this.producto=producto;
 			stock=stockInicial;
 	}
+	public Producto getProducto() {
+		return producto;
+	}
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
 
 	
 	
