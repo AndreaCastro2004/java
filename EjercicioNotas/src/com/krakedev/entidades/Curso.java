29bc6d8a05d9b96081c8f5694749650f6e198543
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-27 21:35:41 -0500

    metodo mostrar

--- EjercicioNotas/src/com/krakedev/entidades/Curso.java
+++ EjercicioNotas/src/com/krakedev/entidades/Curso.java
@@ -1,5 +1,17 @@
 package com.krakedev.entidades;
 
-public class Curso {
+import java.util.ArrayList;
 
+public class Curso {
+	private ArrayList<Estudiante> estudiantes;
+
+	public ArrayList<Estudiante> getEstudiantes() {
+		return estudiantes;
+	}
+
+	public void setEstudiantes(ArrayList<Estudiante> estudiantes) {
+		this.estudiantes = estudiantes;
+	}
+	
+	
 }
--- EjercicioNotas/src/com/krakedev/entidades/Nota.java
+++ EjercicioNotas/src/com/krakedev/entidades/Nota.java
@@ -3,4 +3,27 @@ package com.krakedev.entidades;
 public class Nota {
 	private Materia materia;
 	private String calificacion;
+	
+	//metodos
+	public void mostrar() {
+		System.out.println("Materia:"+materia.getNombre()+" "+"Calificacion:"+calificacion);
+
+	}
+	
+	public Materia getMateria() {
+		return materia;
+	}
+	public void setMateria(Materia materia) {
+		this.materia = materia;
+	}
+	public String getCalificacion() {
+		return calificacion;
+	}
+	public void setCalificacion(String calificacion) {
+		this.calificacion = calificacion;
+	}
+	
+	
+	
+	
 }
