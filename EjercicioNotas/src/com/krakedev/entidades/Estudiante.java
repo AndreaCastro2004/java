4a5c17142255fce03bc279de0ce44b53d0c9e61e
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-27 21:31:20 -0500

    Se crean las clases curso, materia ,estudiantes, nota

---
+++ EjercicioNotas/src/com/krakedev/entidades/Estudiante.java
@@ -0,0 +1,39 @@
+package com.krakedev.entidades;
+
+import java.util.ArrayList;
+
+public class Estudiante {
+	private String nombre;
+	private String apellido;
+	private String cedula;
+	private ArrayList<Nota> notas;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public ArrayList<Nota> getNotas() {
+		return notas;
+	}
+	public void setNotas(ArrayList<Nota> notas) {
+		this.notas = notas;
+	}
+	
+	
+	
+}
---
+++ EjercicioNotas/src/com/krakedev/entidades/Nota.java
@@ -0,0 +1,6 @@
+package com.krakedev.entidades;
+
+public class Nota {
+	private Materia materia;
+	private String calificacion;
+}
