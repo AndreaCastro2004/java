6b999ccdde9ac5294581a8da1bafdcf6a5d3c459
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-28 08:17:45 -0500

    metodoBuscarEstudiante por cedula en class Curso

--- EjercicioNotas/src/com/krakedev/entidades/Curso.java
+++ EjercicioNotas/src/com/krakedev/entidades/Curso.java
@@ -4,6 +4,17 @@ import java.util.ArrayList;
 
 public class Curso {
 	private ArrayList<Estudiante> estudiantes;
+	
+	//metodos
+	public String buscarEstudiantePorCedula(Estudiante estudiante) {
+		Estudiante estudiante1 = new Estudiante();
+		if(estudiante.getCedula().equals(estudiante1.getCedula())) {
+			System.out.println("El estudiante no se encuentra en el curso");
+		}
+		return null;
+		
+		
+	}
 
 	public ArrayList<Estudiante> getEstudiantes() {
 		return estudiantes;
