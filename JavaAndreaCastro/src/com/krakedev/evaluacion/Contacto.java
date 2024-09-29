0471a6228cd025a561b91fb9e50372be2c55eefe
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-09-29 17:56:39 -0500

    agrego testContacto2 y modifico metodo imprimir de Contacto

--- JavaAndreaCastro/src/com/krakedev/evaluacion/Contacto.java
+++ JavaAndreaCastro/src/com/krakedev/evaluacion/Contacto.java
@@ -5,49 +5,56 @@ public class Contacto {
 	private String nombre;
 	private String apellido;
 	private Direccion direccion;
-	//metodo constructor
+
+	// metodo constructor
 	public Contacto(String cedula, String nombre, String apellido) {
 		this.cedula = cedula;
 		this.nombre = nombre;
 		this.apellido = apellido;
 	}
-	//metodos
+
+	// metodos
 	public void imprimir() {
-		System.out.println("Cedula:"+cedula);
-		System.out.println("Nombre:"+nombre);
-		System.out.println("Apellido:"+apellido);
-		System.out.println("Direccion:");
-		System.out.println("Calle Principal:"+direccion.getCallePrincipal());
-		System.out.println("Calle Secundaria:"+direccion.getCalleSecundaria());
+		System.out.println("***" + nombre + " " + apellido + "****");
+		if (direccion != null) {
+			System.out.println("Direccion:" + direccion.getCallePrincipal() + " y " + direccion.getCalleSecundaria());
+		} else {
+			System.out.println("No tiene asociada una direccion");
+		}
 
 	}
-	
-	//getter and setters
+
+	// getter and setters
 	public String getCedula() {
 		return cedula;
 	}
+
 	public void setCedula(String cedula) {
 		this.cedula = cedula;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public Direccion getDireccion() {
 		return direccion;
 	}
+
 	public void setDireccion(Direccion direccion) {
 		this.direccion = direccion;
 	}
-	
-	
-	
+
 }
