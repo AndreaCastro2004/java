f62de7fc033abb71f5c44d960b07046ee4ad2842
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-10-10 13:26:55 -0500

    ejemplos logs and maven

---
+++ EjemploLogs/src/com/krakedev/ejercicios/Admin.java
@@ -0,0 +1,17 @@
+package com.krakedev.ejercicios;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Admin {
+	private Logger loggert=LogManager.getLogger(Admin.class);
+	
+	public void agregar() {
+		loggert.info("mensaje de info");
+		loggert.warn("mensaje a warn");
+		loggert.trace("mensaje a trace");
+		loggert.error("mensaje a error");
+		loggert.debug("mensaje de debug");
+	}
+	
+}
