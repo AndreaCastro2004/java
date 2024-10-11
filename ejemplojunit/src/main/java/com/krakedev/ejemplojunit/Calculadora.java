0b12db7a89d06774a05ed32a74bcc6923ab09c66
AndreaCastro2004 <shinimorimiyo@gmail.com>
2024-10-10 21:51:41 -0500

    Reto 30 ejercicio maven y video

---
+++ ejemplojunit/src/main/java/com/krakedev/ejemplojunit/Calculadora.java
@@ -0,0 +1,13 @@
+package com.krakedev.ejemplojunit;
+
+public class Calculadora {
+	public int sumar(int numero1,int numero2) {
+		int resultado=numero1+numero2;
+		return resultado;
+	}
+	public int restar(int numero1,int numero2) {
+		int resultado=numero1-numero2;
+		return resultado;
+		
+	}
+}
